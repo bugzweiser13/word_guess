7fd2bfbcb3cfd0a2635d43c181f82a58f19eab34
bugzweiser <bugzweiser@gmail.com>
2019-05-05 15:26:44 -0700

    responsive layout progress

--- assets/js/game1.js
+++ assets/js/game1.js
@@ -14,3 +14,36 @@ var losses = 0;
 var guessesRemaining = 9;
 
 //functions
+
+//start functions
+
+function Game() {
+    //word generation
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    //
+    lettersOfWord = randomWord.split("");
+    //
+    blanks = lettersOfWord.length;
+
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    //show in HTML
+    document.getElementById("word").innerHTML = " " + blanksAndCorrect.join(" ");
+
+    //log to console
+    console.log(randomWord);
+    console.log(lettersOfWord);
+    console.log(blanks);
+    console.log(blanksAndCorrect);
+}
+
+function reset() {
+    guessesRemaining = 9;
+    wrongGuess = [];
+    blanksAndCorrect = [];
+    Game()
+}
+
+//letters/compare function
